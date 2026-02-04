b34c9e30a6b58cdf8530d24de6b6d870ae32f0b5
Andrey Ageev <282305@niuitmo.ru>
2026-02-04 18:50:33 +0900

    actualize readme

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
 export default defineConfig({
-  site: "https://yourusername.github.io",
+  site: "https://andreyageev111.github.io",
   base: "/tomochan-house",
   integrations: [react()],
 });
