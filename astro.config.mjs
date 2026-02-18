8a18c16a9ea7c6f6dc225dd19902c47bc5592b56
Andrey Ageev <282305@niuitmo.ru>
2026-02-18 09:57:35 +0900

    style fix and add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://tomochan-house.jp",
   base: "/",
-  integrations: [react()],
+  integrations: [
+    react(),
+    sitemap({
+      // Использовать sitemap для SPA с якорями
+      filter: (page) => !page.includes("#"),
+      // Включить все страницы
+      lastmod: new Date(),
+    }),
+  ],
   vite: {
     build: {
       cssCodeSplit: true,
